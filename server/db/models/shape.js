6f275cc74f615fff0533d92edc78c0b71b3e6504
June Hosoya <junehosoya@gmail.com>
2019-02-26 17:37:53 -0500

    added Description to Shape model

--- server/db/models/shape.js
+++ server/db/models/shape.js
@@ -3,6 +3,10 @@ const db = require('../db')
 
 const Shape = db.define('shape', {
   name: Sequelize.STRING,
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: true
+  },
   size: {
     type: Sequelize.STRING,
     validate: {

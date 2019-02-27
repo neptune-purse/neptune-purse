5265afaa46b419ecbcf2ea9d53d7a981bbb61959
June Hosoya <junehosoya@gmail.com>
2019-02-27 17:19:46 -0500

    feat(client/Component):Created All Product Component and eachComponent to render on All Product Page

--- client/app.js
+++ client/app.js
@@ -1,15 +1,32 @@
-import React from 'react'
-
+import React, {Component} from 'react'
+import {withRouter} from 'react-router-dom'
+import {connect} from 'react-redux'
 import {Navbar} from './components'
 import Routes from './routes'
 
-const App = () => {
-  return (
-    <div>
-      <Navbar />
-      <Routes />
-    </div>
-  )
+//-----------
+
+/*
+IMPORT:
+THUNK CREATOR for:
+- fetch all products
+*/
+class App extends Component {
+  componentDidMount() {
+    this.props.loadProducts()
+  }
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <Routes />
+      </div>
+    )
+  }
 }
 
-export default App
+const mapDispatchToProps = dispatch => ({
+  //CHECK THENAME OF THE THUNK CREATOR
+  loadProducts: () => dispatch(fetchProducts())
+})
+export default withRouter(connect(null, mapDispatchToProps)(App))

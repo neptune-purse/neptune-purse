9ad13cc4d81d7f82111a2cacf1539ae76a366450
MikaelaSheldt <msheldt@gmail.com>
2019-02-27 18:50:55 -0500

    Iss#4 (#18)
    * fix(model/user): added relevant fields
    
    * feat(model/guest) and fix(model/index): includes guest model
    
    * feat(models/cart): will act as order model as well, fix(models/guest): deleted guest model
    
    * feat(models/cart/user): working on the design and functionality. not final
    
    * fix(model/index): fixed import mistake
    
    * fix(models/cart): added }
    
    * fix(model/user): allowNull for names
    
    * Update user.js

---
+++ server/db/models/cart.js
@@ -0,0 +1,16 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+// a order is essentially a cart that includes (eagerloads) all associated shapeId's
+
+const Cart = db.define('cart', {
+  //quantity: Sequelize.INTEGER,
+  name: {
+    type: Sequelize.STRING,
+    validate: {
+      isIn: [['order', 'cart']]
+    }
+  }
+})
+
+module.exports = Cart

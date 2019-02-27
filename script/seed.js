fa34c0adeb31f285d0db0c84b47b104f5199b369
Miyuki Abe <miyuki0809@gmail.com>
2019-02-26 19:48:02 -0500

    #8-create-seeding

--- script/seed.js
+++ script/seed.js
@@ -1,18 +1,237 @@
 'use strict'
 
 const db = require('../server/db')
-const {User} = require('../server/db/models')
+const {Shape} = require('../server/db/models')
+
+const shapes = [
+  {
+    name: 'Circle Blue',
+    description: 'TBA',
+    size: 'small',
+    color: 'Blue',
+    price: 5,
+    imageUrl: '/img/products/blue_circle.png'
+  },
+  {
+    name: 'Circle Blue',
+    description: 'TBA',
+    size: 'medium',
+    color: 'Blue',
+    price: 15,
+    imageUrl: '/img/products/blue_circle.png'
+  },
+  {
+    name: 'Circle Blue',
+    description: 'TBA',
+    size: 'large',
+    color: 'Blue',
+    price: 25,
+    imageUrl: '/img/products/blue_circle.png'
+  },
+  {
+    name: 'Circle Red',
+    description: 'TBA',
+    size: 'small',
+    color: 'Red',
+    price: 5,
+    imageUrl: '/img/products/red_circle.png'
+  },
+  {
+    name: 'Circle Red',
+    description: 'TBA',
+    size: 'medium',
+    color: 'Red',
+    price: 15,
+    imageUrl: '/img/products/red_circle.png'
+  },
+  {
+    name: 'Circle Red',
+    description: 'TBA',
+    size: 'large',
+    color: 'Red',
+    price: 25,
+    imageUrl: '/img/products/red_circle.png'
+  },
+  {
+    name: 'Circle Yellow',
+    description: 'TBA',
+    size: 'small',
+    color: 'Yellow',
+    price: 5,
+    imageUrl: '/img/products/yellow_circle.png'
+  },
+  {
+    name: 'Circle Yellow',
+    description: 'TBA',
+    size: 'medium',
+    color: 'Yellow',
+    price: 15,
+    imageUrl: '/img/products/yellow_circle.png'
+  },
+  {
+    name: 'Circle Yellow',
+    description: 'TBA',
+    size: 'large',
+    color: 'Yellow',
+    price: 25,
+    imageUrl: '/img/products/yellow_circle.png'
+  },
+  {
+    name: 'Square Blue',
+    description: 'TBA',
+    size: 'small',
+    color: 'Blue',
+    price: 5,
+    imageUrl: '/img/products/blue_square.png'
+  },
+  {
+    name: 'Square Blue',
+    description: 'TBA',
+    size: 'medium',
+    color: 'Blue',
+    price: 15,
+    imageUrl: '/img/products/blue_square.png'
+  },
+  {
+    name: 'Square Blue',
+    description: 'TBA',
+    size: 'large',
+    color: 'Blue',
+    price: 25,
+    imageUrl: '/img/products/blue_square.png'
+  },
+  {
+    name: 'Square Red',
+    description: 'TBA',
+    size: 'small',
+    color: 'Red',
+    price: 5,
+    imageUrl: '/img/products/red_square.png'
+  },
+  {
+    name: 'Square Red',
+    description: 'TBA',
+    size: 'medium',
+    color: 'Red',
+    price: 15,
+    imageUrl: '/img/products/red_square.png'
+  },
+  {
+    name: 'Square Red',
+    description: 'TBA',
+    size: 'large',
+    color: 'Red',
+    price: 25,
+    imageUrl: '/img/products/red_square.png'
+  },
+  {
+    name: 'Square Yellow',
+    description: 'TBA',
+    size: 'small',
+    color: 'Yellow',
+    price: 5,
+    imageUrl: '/img/products/yellow_square.png'
+  },
+  {
+    name: 'Square Yellow',
+    description: 'TBA',
+    size: 'medium',
+    color: 'Yellow',
+    price: 15,
+    imageUrl: '/img/products/yellow_square.png'
+  },
+  {
+    name: 'Square Yellow',
+    description: 'TBA',
+    size: 'large',
+    color: 'Yellow',
+    price: 25,
+    imageUrl: '/img/products/yellow_square.png'
+  },
+  {
+    name: 'Triangle Blue',
+    description: 'TBA',
+    size: 'small',
+    color: 'Blue',
+    price: 5,
+    imageUrl: '/img/products/blue_triangle.png'
+  },
+  {
+    name: 'Triangle Blue',
+    description: 'TBA',
+    size: 'medium',
+    color: 'Blue',
+    price: 15,
+    imageUrl: '/img/products/blue_triangle.png'
+  },
+  {
+    name: 'Triangle Blue',
+    description: 'TBA',
+    size: 'large',
+    color: 'Blue',
+    price: 25,
+    imageUrl: '/img/products/blue_triangle.png'
+  },
+  {
+    name: 'Triangle Red',
+    description: 'TBA',
+    size: 'small',
+    color: 'Red',
+    price: 5,
+    imageUrl: '/img/products/red_triangle.png'
+  },
+  {
+    name: 'Triangle Red',
+    description: 'TBA',
+    size: 'medium',
+    color: 'Red',
+    price: 15,
+    imageUrl: '/img/products/red_triangle.png'
+  },
+  {
+    name: 'Triangle Red',
+    description: 'TBA',
+    size: 'large',
+    color: 'Red',
+    price: 25,
+    imageUrl: '/img/products/red_triangle.png'
+  },
+  {
+    name: 'Triangle Yellow',
+    description: 'TBA',
+    size: 'small',
+    color: 'Yellow',
+    price: 5,
+    imageUrl: '/img/products/yellow_triangle.png'
+  },
+  {
+    name: 'Triangle Yellow',
+    description: 'TBA',
+    size: 'medium',
+    color: 'Yellow',
+    price: 15,
+    imageUrl: '/img/products/yellow_triangle.png'
+  },
+  {
+    name: 'Triangle Yellow',
+    description: 'TBA',
+    size: 'large',
+    color: 'Yellow',
+    price: 25,
+    imageUrl: '/img/products/yellow_triangle.png'
+  }
+]
 
 async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
 
-  const users = await Promise.all([
-    User.create({email: 'cody@email.com', password: '123'}),
-    User.create({email: 'murphy@email.com', password: '123'})
-  ])
+  await Promise.all(
+    shapes.map(shape => {
+      return Shape.create(shape)
+    })
+  )
 
-  console.log(`seeded ${users.length} users`)
   console.log(`seeded successfully`)
 }
 

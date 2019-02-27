9f0ac178cc320227816a18fdd8f81b62cfe2d474
June Hosoya <junehosoya@gmail.com>
2019-02-27 13:10:24 -0500

    feat(client/components): Created new component and route for AllProducts

---
+++ client/components/allProduct.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+const AllProduct = () => (
+  <div>
+    <h1>We are at Product Page</h1>
+  </div>
+)
+
+export default AllProduct

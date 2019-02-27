6e3d24b4e0621ce4af213c2414e54afb60ab51e9
June Hosoya <junehosoya@gmail.com>
2019-02-27 17:19:46 -0500

    feat(client/Component):Created All Product Component and eachComponent to render on All Product Page

--- client/components/allProduct.js
+++ client/components/allProduct.js
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import {Link, withRouter} from 'react-router-dom'
 
-const AllProduct = () => (
-  <div>
-    <h1>We are at Product Page</h1>
-  </div>
+//----------------------------
+import EachProduct from './eachProduct'
+
+//NEED TO IMPORT FROM STORE:
+/*
+- state of product
+*/
+
+class AllProduct extends Component {
+  constructor(props) {
+    super(props)
+  }
+  // addButton() {
+  //   this.state
+  // }
+  render() {
+    return (
+      <div className="allProductPage">
+        {props.allProducts.map(product => (
+          <section>
+            <EachProduct product={product} />
+          </section>
+        ))}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  //check the name of the all products in the store
+  allProducts: state.products,
+  EachProduct: state.product
+})
+
+export default withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(AllProduct)
 )
-
-export default AllProduct
--- client/components/index.js
+++ client/components/index.js
@@ -6,4 +6,5 @@
 export {default as Navbar} from './navbar'
 export {default as UserHome} from './user-home'
 export {default as AllProduct} from './allProduct'
+export {default as EachProduct} from './eachProduct'
 export {Login, Signup} from './auth-form'

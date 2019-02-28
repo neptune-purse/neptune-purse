19608707ab19da8b3133d7834a7e0d63fda9a815
Cassie Rosner <cerosner@gmail.com>
2019-02-28 17:51:00 -0500

    fix(models/user): data types

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -4,16 +4,16 @@ const db = require('../db')
 
 const User = db.define('user', {
   firstName: {
-    type: Sequelize.STRING
-    //allowNull: false
+    type: Sequelize.STRING,
+    allowNull: false
   },
   lastName: {
-    type: Sequelize.STRING
-    //allowNull: false
+    type: Sequelize.STRING,
+    allowNull: false
   },
   imageUrl: {
     type: Sequelize.STRING,
-    defaultValue: '../../../public/favicon.ico'
+    defaultValue: '/favicon.ico'
   },
   email: {
     type: Sequelize.STRING,
--- server/db/models/user.spec.js
+++ server/db/models/user.spec.js
@@ -15,6 +15,8 @@ describe('User model', () => {
 
       beforeEach(async () => {
         cody = await User.create({
+          firstName: 'Cody',
+          lastName: 'Puppy',
           email: 'cody@puppybook.com',
           password: 'bones'
         })

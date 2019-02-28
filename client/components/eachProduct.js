fef457bb282819deb7b1890a974771376a60da71
June Hosoya <junehosoya@gmail.com>
2019-02-28 11:14:58 -0500

    CSS/allProductPage: added CSS style

--- client/components/eachProduct.js
+++ client/components/eachProduct.js
@@ -5,14 +5,14 @@ import {Link, withRouter} from 'react-router-dom'
 const EachProduct = props => {
   const product = props.product
   return (
-    <div className="eachProduct">
+    <div className="wrapper">
       <ul id="eachProduct">
         <li>
-          <Link to={`/product/${product.id}`}>
+          <Link to={`/shapes/${product.id}`}>
             <img className="productImg" src={product.imageUrl} alt="image" />
             {product.name}
           </Link>
-          {product.price}
+          <p>Price:{product.price} </p>
           {/* <button className="addButton" onClick={this.addButton} /> */}
         </li>
       </ul>

36881aaeb82ed3e351f4320f0a6b7fa91e748e94
Mikaela Sheldt <msheldt@gmail.com>
2019-03-03 17:27:49 -0500

    NOT tracking bundle.js, feat(api/auth): create order on sign-up

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -3,12 +3,12 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const User = db.define('user', {
-  firstName: {
-    type: Sequelize.STRING
-  },
-  lastName: {
-    type: Sequelize.STRING
-  },
+  // firstName: {
+  //   type: Sequelize.STRING
+  // },
+  // lastName: {
+  //   type: Sequelize.STRING
+  // },
   imageUrl: {
     type: Sequelize.STRING,
     defaultValue: '/favicon.ico'

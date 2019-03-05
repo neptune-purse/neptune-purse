a2dc22ebbee14f0a871479657e30c1f18c20304e
June Hosoya <junehosoya@gmail.com>
2019-03-04 21:14:19 -0500

    feat(Cart/Component): Product Rendering on the front page - without UserId association

--- client/components/shoppingCart.js
+++ client/components/shoppingCart.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import {connect} from 'react-redux'
 
 const ShoppingCart = props => {
+  console.log(props)
+  const {cart} = props
+  console.log(cart)
   return (
     <div>
       <h1>Current Orders</h1>
+      {cart.map(items => (
+        <div key={items.id} className="oneShape">
+          <img src={items.imageUrl} className="shapeImg" alt={items.name} />
+          <h6>Price: ${items.price}</h6>
+        </div>
+      ))}
     </div>
   )
 }
 
-export default ShoppingCart
+const mapStateToProps = state => ({
+  cart: state.cart.items
+})
+
+export default connect(mapStateToProps)(ShoppingCart)
